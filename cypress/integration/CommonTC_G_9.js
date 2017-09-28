1800e319c1b8bae10d39384b9edfcae9b8ce204b
Nguyen Duc Thuan <ndthuanuet@gmail.com>
2017-09-28 15:21:12 +0700

    CommonTC_G_9

--- cypress/integration/CommonTC_G_9.js
+++ cypress/integration/CommonTC_G_9.js
@@ -5,17 +5,35 @@ describe("Kiem tra thuc hien chuc nang chinh cua man hinh khi nhan Enter", funct
 
     _.forEach(Cypress.env('users'), function (user) {
         context("User: " + user.role, function () {
-            it("dang nhap", function () {
-                cy.visit(Cypress.env('LOGIN_URL'));
-                cy.get('input[name=email]').type(user.user.email);
-                cy.get('input[name=password]').type(user.user.password);
-                cy.get('button[type=submit]').click();
-            });
+            _.forEach(user.urls, function (url) {
 
-            it("Kiem tra grid trong /main/admin/administrators/employments", function () {
-                cy.visit('/main/admin/administrators/employments');
-                console.log("123");
-                cy.get('table');
+                it("Kiem tra grid trong " + url, function () {
+                    cy.visit(Cypress.env('LOGIN_URL'));
+                    cy.get('input[name=email]').type(user.user.email);
+                    cy.get('input[name=password]').type(user.user.password);
+                    cy.get('button[type=submit]').click();
+                    cy.wait(1000);
+                    cy.visit(url);
+                    cy.wait(1000);
+                    cy.get('table tr td').each(function ($el, index, $list) {
+                        // console.log($el.text(), isNaN($el.text()));
+                        if(!isNaN(parseInt($el.text()))) {
+                            // console.log($el.text(), parseInt($el.text()));
+                            expect($el).to.have.css('text-align', 'right');
+                        } else {
+                            var date = new Date($el.text());
+                            if (date instanceof Date && !isNaN(date.valueOf())) {
+                                expect($el).to.have.css('text-align', 'center');
+                            }
+                            expect($el).to.have.css('text-align', 'left');
+                        }
+                    });
+
+                    cy.get('.page-header .title').each(function ($el, index, $list) {
+                        expect($el).to.have.css('text-align', 'left');
+                        expect($el).to.have.css('font-weight', 'bold');
+                    })
+                });
             });
 
 
